d5b69fad64b0c1f739929da3e6c948cf0ddf7479
wwwpontocom <wwwpontoscom@gmail.com>
2026-02-19 15:12:43 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,46 @@
+const CACHE_NAME = 'msa-gm-central-v1';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './style.css',
+    './data_fases.js',
+    './data_extras.js',
+    './data_logs.js',
+    './main.js',
+    'https://www.gstatic.com/firebasejs/9.1.3/firebase-app-compat.js',
+    'https://www.gstatic.com/firebasejs/9.1.3/firebase-database-compat.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
+];
 
+// Instalação: Salva os arquivos no cache
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+// Ativação: Limpa caches antigos
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Estratégia: Tenta a rede, se falhar, usa o cache
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
+    );
+});
